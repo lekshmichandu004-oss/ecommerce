1bc0ba95c0cfbc6c6340297fecf87e36c4b00bee
lekshmichandu004-oss <lekshmichandu004@gmail.com>
2026-09-18 11:21:21 +0530

    sets user permission

--- core/settings.py
+++ core/settings.py
@@ -68,7 +68,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 ROOT_URLCONF = 'core.urls'
